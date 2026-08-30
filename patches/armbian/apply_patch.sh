58977c1bde6e44d836a97785bb6e1501d0513c11
artm1904 <artmsavchenko@yandex.ru>
2026-08-30 15:12:08 +0300

    patch: for armbian upstream git-repo

---
+++ patches/armbian/apply_patch.sh
@@ -0,0 +1,2 @@
+git -C third_party/armbian-build apply --check ../../patches/armbian/0001-enable-ov5647-sun60iw2.patch
+git -C third_party/armbian-build apply ../../patches/armbian/0001-enable-ov5647-sun60iw2.patch
